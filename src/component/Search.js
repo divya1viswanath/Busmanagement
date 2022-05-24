e6f73e2c15a9dc899fa1e45594616230afc8bf02
divya1viswanath <divyarugmini798@gmail.com>
2022-05-24 22:31:52 +0530

    search completed

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+ var [busname,setBusname]=useState("")
+
+ const subData =() =>{
+     const data={ "busname":busname}
+     console.log(data)
+ }
+ 
   return (
-    <div>Search
-        
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                    
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label for="" className="form-label">Bus Name</label>
+                            <input onChange={(e)=>{setBusname(e.target.value)}} type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={subData} className="btn btn-primary">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
